b5a9addfd5741f63082dd6e6ea27db6846462bd7
thusspokedata <thusspokedata@protonmail.com>
2024-08-12 18:54:42 +0200

    section8: Acciones e interfaces

--- src/products/index.ts
+++ src/products/index.ts
@@ -1,12 +1,11 @@
-
-
-
 export { ProductCard } from './components/ProductCard';
 export { ProductList } from './components/ProductList';
 
-
 export { StoreLayout } from './layout/StoreLayout';
 
+export type { Product } from './interfaces/product';
+
+export { productsApi } from './api/productsApi';
 
 export { CompleteListPage } from './pages/CompleteListPage';
 export { MensPage } from './pages/MensPage';
---
+++ src/products/services/actions.ts
@@ -0,0 +1,10 @@
+import { type Product, productsApi } from '..';
+
+interface GetProductsOptions {
+  filterKey?: string;
+}
+
+export const getProducts = async ({ filterKey }: GetProductsOptions) => {
+  const { data } = await productsApi.get<Product[]>('/products');
+  return data;
+};
