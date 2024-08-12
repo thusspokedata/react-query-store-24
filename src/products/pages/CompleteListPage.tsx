7e63d27907e5c30c2d58093a1e5a53561b6e79fd
thusspokedata <thusspokedata@protonmail.com>
2024-08-12 23:45:58 +0200

    section8: Artículos de Hombre y Mujer

--- src/products/pages/CompleteListPage.tsx
+++ src/products/pages/CompleteListPage.tsx
@@ -1,12 +1,12 @@
 import { ProductList, useProducts } from '..';
 
 export const CompleteListPage = () => {
-  const {  products } = useProducts({});
+  const { isLoading, products } = useProducts({});
 
   return (
     <div className="flex-col">
       <h1 className="text-2xl font-bold">Todos los productos</h1>
-
+      {isLoading && <div>Cargando...</div>}
       <ProductList products={products} />
     </div>
   );
--- src/products/services/actions.ts
+++ src/products/services/actions.ts
@@ -4,7 +4,14 @@ interface GetProductsOptions {
   filterKey?: string;
 }
 
+const sleep = (seconds: number = 0): Promise<boolean> =>
+  new Promise((resolve) => setTimeout(() => resolve(true), seconds * 1000));
+
 export const getProducts = async ({ filterKey }: GetProductsOptions) => {
-  const { data } = await productsApi.get<Product[]>('/products');
+  await sleep(2);
+
+  const filterUrl = filterKey ? `category=${filterKey}` : '';
+
+  const { data } = await productsApi.get<Product[]>(`/products?${filterUrl}`);
   return data;
 };
